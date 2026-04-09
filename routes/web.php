314b1b333925976fe8120dbfb2b1e9f636ad5bad
Scyla001 <rimuru05192006@gmail.com>
2026-04-09 16:26:28 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WordController;
 
-// Redirect home to view
 Route::get('/', function () {
     return redirect()->route('words-view');
 });
 
-// Custom view route
 Route::get('/view', [WordController::class, 'index'])->name('words-view');
 
-// Resource routes (optional, for CRUD forms)
 Route::resource('words', WordController::class)->except(['index']);
